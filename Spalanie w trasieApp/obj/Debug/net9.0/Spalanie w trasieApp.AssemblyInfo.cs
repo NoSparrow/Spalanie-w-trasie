1521fc54d161730e5210f6fe5db4b046293422e5
Sparrow <wroblewski.m@int.pl>
2025-06-19 21:39:20 +0200

    Update 2

--- Spalanie w trasieApp/obj/Debug/net9.0/Spalanie w trasieApp.AssemblyInfo.cs
+++ Spalanie w trasieApp/obj/Debug/net9.0/Spalanie w trasieApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spalanie w trasieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e0de16b7bb4260381e23f3ed80f4a2c4eab2503")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c1d330ad780df25162dc76f7dd7d80bccfafd56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spalanie w trasieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spalanie w trasieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
